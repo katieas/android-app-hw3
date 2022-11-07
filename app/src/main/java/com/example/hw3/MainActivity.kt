b36c845c5310ca3e1ada2ad97263277c61bf9968
katie <katieas808@gmail.com>
2022-11-06 23:15:27 -0800

    added zybooks pizza party and lightsout code

--- app/src/main/java/com/example/hw3/MainActivity.kt
+++ app/src/main/java/com/example/hw3/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.hw3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+//val SLICES_PER_PIZZA = 8 //getInteger(R.integer.slices_per_pizza)
+val TAG = "MainActivity" //getString(R.string.tag)
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/example/hw3/pizzaparty/PizzaParty.kt
@@ -0,0 +1,110 @@
+package com.example.hw3.pizzaparty
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hw3.R
+
+private const val TAG = "Lifecycle"
+private const val KEY_TOTAL_PIZZAS = "totalPizzas"
+
+class PizzaPartyActivity : AppCompatActivity() {
+
+    private lateinit var numAttendEditText: EditText
+    private lateinit var numPizzasTextView: TextView
+    private lateinit var howHungryRadioGroup: RadioGroup
+    private var totalPizzas = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pizza_party)
+        numAttendEditText = findViewById(R.id.num_attend_edit_text)
+        numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
+
+        //sets initial value to 0 rather than %d
+        numPizzasTextView.text = resources.getString(R.string.total_pizzas, 0)
+
+        howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+
+        Log.d(TAG, "onCreate")
+
+        //Do we need to restore saveInstanceState
+        if (savedInstanceState != null) {
+            // we have a saeInstanceState object --> saved data!
+            totalPizzas = savedInstanceState.getInt(KEY_TOTAL_PIZZAS)
+            displayTotal()
+        }
+    }
+
+    private fun displayTotal() {
+        val totalText = getString(R.string.total_pizzas, totalPizzas)
+        numPizzasTextView.text = totalText
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putInt(KEY_TOTAL_PIZZAS, totalPizzas)
+        //outState.putInt("totalPizzas", 10)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        totalPizzas = savedInstanceState.getInt(KEY_TOTAL_PIZZAS)
+        displayTotal()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    fun calculateClick(view: View) {
+
+        // Get the text that was typed into the EditText
+        val numAttendStr = numAttendEditText.text.toString()
+
+        // Convert the text into an integer
+        val numAttend = numAttendStr.toIntOrNull() ?: 0
+
+        // Get hunger level selection
+        val hungerLevel = when (howHungryRadioGroup.getCheckedRadioButtonId()) {
+            R.id.light_radio_button -> PizzaCalculator.HungerLevel.LIGHT
+            R.id.medium_radio_button -> PizzaCalculator.HungerLevel.MEDIUM
+            else -> PizzaCalculator.HungerLevel.RAVENOUS
+        }
+
+        // Get the number of pizzas needed
+        val calc = PizzaCalculator(numAttend, hungerLevel)
+        totalPizzas = calc.totalPizzas
+        displayTotal()
+
+        // Place totalPizzas into the string resource and display
+        val totalText = getString(R.string.total_pizzas, totalPizzas)
+        numPizzasTextView.setText(totalText)
+    }
+}
